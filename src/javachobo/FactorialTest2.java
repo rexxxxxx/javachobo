2e8fd88700172f02d9fd867e29c1d4f73262c509
rexxxxxx <haroo0310@gmail.com>
2016-07-09 22:36:58 +0900

    자기전에

--- src/javachobo/FactorialTest2.java
+++ src/javachobo/FactorialTest2.java
@@ -1,11 +1,18 @@
 package javachobo;
 
+import java.util.Scanner;
+
 public class FactorialTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=21;
+		int n=24;
 		long result = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(" Insert Number (0<n<=20) : > ");
+		n = sc.nextInt();
 		for(int i=1; i<=n;i++){
 			result = factorial(i);
 			if(result ==-1){
